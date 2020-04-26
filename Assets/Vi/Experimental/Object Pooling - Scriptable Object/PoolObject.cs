e3a395c9ab2511e18cee4f7f3aa94c79a0654a81
Vi Tiet <vitiet.programmer@gmail.com>
2020-04-25 21:40:12 -0400

    Reorganized Folder

---
+++ Assets/Vi/Experimental/Object Pooling - Scriptable Object/PoolObject.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace Experimental
+{
+    [CreateAssetMenu(fileName = "New Pool Object", menuName = "Pool Object")]
+    public class PoolObject : ScriptableObject
+    {
+        public string objectName;
+        public GameObject prefab;
+        public int amount;
+        public float activeTime;
+
+
+        private void UpdatePooledObjectInEditor()
+        {
+            if (prefab)
+                objectName = prefab.name;
+            else
+                objectName = "";
+
+            if (amount < 0)
+                amount = 0;
+        }
+
+
+        private void OnValidate()
+        {
+            UpdatePooledObjectInEditor();
+        }
+    }
+}
