209ed53b275791b1166e17c97916f6368982250d
Holidoli <liandongwei@gmail.com>
2020-04-30 13:40:28 -0400

    TrapSystem

---
+++ Assets/William/Script/TrapScriptable.cs
@@ -0,0 +1,13 @@
+﻿
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Trap", menuName = "Trap/Make New Trap", order = 2)]
+public class TrapScriptable : ScriptableObject
+{
+    [SerializeField] GameObject TrapPrefab = null;
+
+    public void SpawnTrap(Transform DropLocation)
+    {
+        Instantiate(TrapPrefab, DropLocation.position, Quaternion.identity);
+    }
+}
---
+++ Assets/William/Script/TrapSystem.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapSystem : MonoBehaviour
+{
+    [SerializeField] TrapScriptable CurrentTrap;
+    [SerializeField] TrapScriptable Trap1;
+    [SerializeField] TrapScriptable Trap2;
+    [SerializeField] Transform TrapSpawnLocation;
+
+    private void Update()
+    {
+        if(Trap1 != null)
+        {
+            CurrentTrap = Trap1;
+        }
+        else if (Trap1 == null && Trap2 != null)
+        {
+            CurrentTrap = Trap2;
+        }
+        else
+        {
+            CurrentTrap = null;
+        }
+
+        if(Input.GetKeyDown(KeyCode.JoystickButton4))
+        {
+            if (CurrentTrap == null) return;
+            CurrentTrap.SpawnTrap(TrapSpawnLocation);
+            if(CurrentTrap == Trap1)
+            {
+                Trap1 = null;
+            }
+            else if(CurrentTrap == Trap2)
+            {
+                Trap2 = null;
+            }
+        }
+    }
+}
